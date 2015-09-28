7e973cdd047b21ea8f6a85145c2bdef01ea8732f
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-09-28 15:15:39 +0500

    Slight change in ServerJs

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ var express = require('express'),
 	app = express();
 
 app
-	.use(express.static(__dirname + "/public"))
-	.use(bodyParser.json())
-	.get('/', function(req, res){
-		res.sendFile(__dirname + '/public/index.html')
+	.use(express.static(__dirname + '/public'))
+	.use(bodyParser.json());
+
+var id = 0;
+var books = {};
+app.post('/books', function(req,res){
+	var book = req.body;
+	book.id = ++id;
+	books[book.id] = book;
+	res.json(book);
+});
+
+
+app
+	.get('*', function(req,res){
+		req.sendFile(__dirname + '/public/index.html');
 	})
 	.listen(3000);
