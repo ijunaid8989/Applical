ed4e3aaecf872bc49d9410990b389e4e06a6afd4
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-09-28 14:18:45 +0500

    Added Profile

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-// define express module
-
 var express = require('express'),
 	bodyParser = require('body-parser'),
 	app = express();
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-
-app.get('*', function(req,res){
-	res.sendFile(__dirname + '/public/index.html');
-});
-
-app.listen(3000);
+app
+	.use(express.static(__dirname + "/public"))
+	.use(bodyParser.json())
+	.get('*', function(req, res){
+		res.sendFile(__dirname + '/public/index.html')
+	})
+	.listen(3000);
